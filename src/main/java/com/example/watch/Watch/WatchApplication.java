d27d65ff6a551df2c2ae92a0509d6fcc74f7e076
CaioXys <caiofer0515@gmail.com>
2026-02-03 23:14:56 -0300

    Aplicando a API

--- src/main/java/com/example/watch/Watch/WatchApplication.java
+++ src/main/java/com/example/watch/Watch/WatchApplication.java
@@ -1,5 +1,7 @@
 package com.example.watch.Watch;
 
+import com.example.watch.Watch.dto.OmdBDTO;
+import com.example.watch.Watch.service.ApiService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +10,15 @@ public class WatchApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WatchApplication.class, args);
-	}
+
+		ApiService apiService = new ApiService();
+		try {
+			OmdBDTO omdBDTO = apiService.omdB("");
+			System.out.println(omdBDTO.getTitle());
+
+		} catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 
 }
