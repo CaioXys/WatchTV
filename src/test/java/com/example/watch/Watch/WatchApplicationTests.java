395093da9fc649f2911706581cc5cc8ec895cd26
CaioXys <caiofer0515@gmail.com>
2026-02-03 22:22:11 -0300

    Initial commit

---
+++ src/test/java/com/example/watch/Watch/WatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.watch.Watch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
