e7ad57419b6a08604a6fcedeac75adaae57573a7
CaioXys <caiofer0515@gmail.com>
2026-02-17 13:56:25 -0300

    Dividindo classes para início de procura de séries.

--- src/main/java/com/example/watch/Watch/WatchApplication.java
+++ src/main/java/com/example/watch/Watch/WatchApplication.java
@@ -1,25 +1,25 @@
 package com.example.watch.Watch;
 
 
-import com.example.watch.Watch.main.Principal;
+import com.example.watch.Watch.main.PrincipalFilme;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WatchApplication implements CommandLineRunner {
-    private final Principal principal;
+    private final PrincipalFilme principalFilme;
 
     public static void main(String[] args) {
         SpringApplication.run(WatchApplication.class, args);
     }
 
-    public WatchApplication(Principal principal) {
-        this.principal = principal;
+    public WatchApplication(PrincipalFilme principalFilme) {
+        this.principalFilme = principalFilme;
     }
 
     @Override
     public void run(String... args) throws Exception {
-        principal.exibeMenu();
+        principalFilme.exibeMenu();
     }
 }
