d3d8f4fb6ed0140e665f41b36c204ac5e8caa378
CaioXys <caiofer0515@gmail.com>
2026-02-19 13:21:53 -0300

    Arrumando problema de classes, criação do banco de dados com migrations, e mudando lógica do looping para não quebrar.

--- src/main/java/com/example/watch/Watch/WatchApplication.java
+++ src/main/java/com/example/watch/Watch/WatchApplication.java
@@ -1,26 +1,25 @@
 package com.example.watch.Watch;
 
 
-import com.example.watch.Watch.main.PrincipalEscolha;
-import com.example.watch.Watch.main.PrincipalFilme;
+import com.example.watch.Watch.main.PrincipalConsole;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WatchApplication implements CommandLineRunner {
-    private final PrincipalEscolha principalEscolha;
+    private final PrincipalConsole principalConsole;
 
     public static void main(String[] args) {
         SpringApplication.run(WatchApplication.class, args);
     }
 
-    public WatchApplication(PrincipalEscolha principalEscolha) {
-        this.principalEscolha = principalEscolha;
+    public WatchApplication(PrincipalConsole principalConsole) {
+        this.principalConsole = principalConsole;
     }
 
     @Override
     public void run(String... args) throws Exception {
-        principalEscolha.escolhaNumero();
+        principalConsole.escolhaNumero();
     }
 }
--- src/main/java/com/example/watch/Watch/model/DadosSeries.java
+++ src/main/java/com/example/watch/Watch/model/DadosSeries.java
@@ -7,7 +7,6 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 public record DadosSeries(@JsonAlias("Title") String titulo,
                           @JsonAlias("Genre") String genero,
                           @JsonAlias("Released") String lancamento,
-                          @JsonAlias("Runtime") String duracao,
                           @JsonAlias("imdbRating") String avaliacao,
                           @JsonAlias("Plot") String sinopse,
                           @JsonAlias("totalSeasons") String temporadas,
